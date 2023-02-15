ded5b58417c175be36dc7f559f4d612e25e5e21c
Gabriel Gómez Mera <Gommergab@IESPIRINGALLA.AD>
2023-02-15 18:53:35 +0100

    Game Manager finalizado

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,81 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    #region variables_publicas
+    public Camara camara;
+    public RawImage imgCam;
+    public Text[] text;
+    #endregion
+    #region variables_privadas
+    private Camaras camaras;
+    private bool cargado = false;
+    #endregion
+
+    void Awake()
     {
-        
+        StartCoroutine(GetQuestions("https://servizos.meteogalicia.gal/mgrss/observacion/jsonCamaras.action"));
+    }
+     void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)&&cargado==true)
+        {
+            cargado = false;
+            camara = camaras.listaCamaras[Random.Range(0, camaras.listaCamaras.Count)];
+            StartCoroutine(DownloadImage(camara.imaxeCamara));
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator GetQuestions(string uri)
     {
-        
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            yield return webRequest.SendWebRequest();
+            if (webRequest.result == UnityWebRequest.Result.Success)
+            {
+                LoadJson(webRequest.downloadHandler.text);
+            }
+            else
+            {
+                Debug.Log("No se puede conectar con la api");
+            }
+        }
     }
+
+    public void LoadJson(string jsonString)
+    {
+        camaras = JsonUtility.FromJson<Camaras>(jsonString);
+        camara = camaras.listaCamaras[Random.Range(0,camaras.listaCamaras.Count)];
+        StartCoroutine(DownloadImage(camara.imaxeCamara));
+    }
+
+
+    IEnumerator DownloadImage(string MediaUrl)
+    {
+       using (UnityWebRequest webRequest = UnityWebRequestTexture.GetTexture(MediaUrl))
+        {
+            yield return webRequest.SendWebRequest();
+            if (webRequest.result == UnityWebRequest.Result.Success)
+            {
+                imgCam.texture = (Texture)((DownloadHandlerTexture)webRequest.downloadHandler).texture;
+
+                text[0].text = camara.concello;
+                text[1].text = camara.dataUltimaAct;
+                text[2].text = camara.idConcello.ToString();
+                text[3].text = camara.identificador.ToString();
+                text[4].text = camara.lat.ToString();
+                text[5].text = camara.lon.ToString();
+                text[6].text = camara.nomeCamara;
+                text[7].text = camara.provincia;
+                cargado = true;
+            }
+            else
+            {
+                Debug.Log("No se puede conectar con la api");
+            }
+        }
+            }
 }
